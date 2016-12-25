b62eea4180b71f7dea97e42d51be4fa826043797
sarvesh-gupta <sarvesh.gupta@hotmail.com>
2016-12-25 15:07:42 +0530

    Packages used
    packages are like namespaces in C#

--- src/HelloWorld/src/Main.java
+++ src/HelloWorld/src/Main.java
@@ -1,7 +1,14 @@
+import net.sarvesh.organized.MainOrg;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello World")
-        ;
+	    System.out.println("Hello World");
+
+	    printOrganized();
+    }
+
+    public static void printOrganized(){
+        MainOrg n = new MainOrg();
     }
 }
---
+++ src/HelloWorld/src/net/sarvesh/organized/MainOrg.java
@@ -0,0 +1,10 @@
+package net.sarvesh.organized;
+
+/**
+ * Created by Sarvesh on 12/25/2016.
+ */
+public class MainOrg {
+    public MainOrg(){
+        System.out.println("Organized using packages (namespace)");
+    }
+}
