591a785674f8da7a7e00ed69761be92747c7fc33
learn-beginner <sarvesh.gupta@hotmail.com>
2017-01-03 19:17:41 +0530

    dfs connected components in undirected graph in progress

--- src/graph/dfs/src/net/sarvesh/graph/Main.java
+++ src/graph/dfs/src/net/sarvesh/graph/Main.java
@@ -1,8 +1,68 @@
 package net.sarvesh.graph;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InterruptedIOException;
+import java.util.Scanner;
+
 public class Main {
 
+    static int[][] adj;
+    static boolean[] visited;
+
+    static void dfs(int currentNode) {
+        visited[currentNode] = true;
+        for (int i = 0; i < adj.length; i++) {
+            if (visited[adj[currentNode][i]] == false) {
+                dfs(adj[currentNode][i]);
+            }
+        }
+    }
+
+    static void initialize() {
+        for (int i = 0; i < visited.length; i++) {
+            visited[i] = false;
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        try {
+            int nodes, edges, x, y, connectedComponents = 0;
+            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+            nodes = Integer.parseInt(stdin.readLine());
+            edges = Integer.parseInt(stdin.readLine());
+            adj = new int[nodes][nodes];
+            visited = new boolean[nodes];
+            for (int i = 0; i < edges; ++i) {
+                String line = stdin.readLine();
+                String[] e = line.split(" ");
+//                adj[Integer.parseInt(e[0])][Integer.parseInt(e[1])] = Integer.parseInt(e[0]);
+//                adj[Integer.parseInt(e[1])][Integer.parseInt(e[0])] = Integer.parseInt(e[1]);
+                adj[Integer.parseInt(e[0]) - 1][Integer.parseInt(e[1]) - 1] = 1;
+                adj[Integer.parseInt(e[1]) - 1][Integer.parseInt(e[0]) - 1] = 1;
+            }
+
+            initialize();
+
+            for (int i = 0; i < nodes; i++) {
+                if (visited[i] == false) {
+                    dfs(i);
+                    connectedComponents++;
+                }
+            }
+
+            System.out.println("Number of connected components: " + connectedComponents);
+
+            for (int i = 0; i < adj.length; i++) {
+                for (int j = 0; j < adj[i].length; j++) {
+                    System.out.print(adj[i][j]);
+                }
+                System.out.println();
+            }
+
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
